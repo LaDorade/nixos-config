db510d7a8df56e3b7149d665570a1e4fd21def8c
matysse <mathisjung02@gmail.com>
2025-08-01 22:07:19 +0200

    fixed neovim duplicate with nixvim

---
+++ home/modules/neovim.nix
@@ -0,0 +1,52 @@
+{ pkgs, lib, config, home, ... }:
+{
+  options = {
+    nixvim.enable = lib.mkEnableOption "Enable nixvim";
+  };
+
+  config = { 
+    home.packages = lib.mkIf (!config.nixvim.enable) [ pkgs.neovim ];
+    programs.nixvim = lib.mkIf config.nixvim.enable {
+      enable = true;
+      opts = {
+        number = true;
+        relativenumber = true;
+        shiftwidth = 2; # tabwith is 2
+      };
+      plugins = {
+        nix.enable = true;
+        comment.enable = true;
+        oil = { enable = true; };
+        treesitter = {
+          enable = true;
+          settings = {
+            ensure_installed = [ "lua" "python" "rust" "nix" "bash" "json" ];
+            highlight = {
+              enable = true;
+              # additionalVimRegexHighlighting = false;
+            };
+          };
+
+          grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+            bash
+            json
+            lua
+            make
+            markdown
+            nix
+            regex
+            toml
+            vim
+            vimdoc
+            xml
+            yaml
+          ];
+        };
+      };
+      colorschemes.everforest.enable = true;
+      plugins.lualine.enable = true;
+
+      extraPackages = with pkgs; [ wl-clipboard ];
+    };
+  };
+}
